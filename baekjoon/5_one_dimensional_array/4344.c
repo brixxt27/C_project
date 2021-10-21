b7cdd27553befbbf4ce4a1b8c024a945317cff01
brixxt27 <brixxt27@gmail.com>
2021-10-21 18:38:55 +0900

    end O.D.A.

--- baekjoon/5_one_dimensional_array/4344.c
+++ baekjoon/5_one_dimensional_array/4344.c
@@ -2,5 +2,34 @@
 
 int		main(void)
 {
+	int		testcase;
+	int		i, j;
+	int		n;
+	int		score[1000];
+	int		sum = 0;
+	double	avg;
+	int 	cnt = 0;
+	double	upavg;
+	
+	scanf("%d", &testcase);
+	for (i = 0; i < testcase; i++)
+	{
+		scanf("%d", &n);
+		for (j = 0; j < n; j++)
+		{
+			scanf("%d", &score[j]);
+			sum += score[j];
+		}
+		avg = sum / (double)n;
+		for (j = 0; j < n; j++)
+		{
+			if (avg < score[j])
+				cnt++;
+		}
+		upavg = cnt / (double)n * 100;
+		printf("%.3lf%%\n", upavg);
+		cnt = 0;
+		sum = 0;
+	}
 	return 0;
 }
