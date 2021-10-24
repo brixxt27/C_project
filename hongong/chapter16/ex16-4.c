7f5cfd0479e4e70bc7274eb0db0dc8579a2b291f
brixxt27 <brixxt27@gmail.com>
2021-10-25 01:54:47 +0900

    pigonhae

--- hongong/chapter16/ex16-4.c
+++ hongong/chapter16/ex16-4.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	char temp[80];
+	char	temp[80];
+	char	*sp[3];
+	int		i;
 
-	printf("문자열을 입력하세요 : ");
-	get(temp);	
+	for (i = 0; i < 3; i++)
+	{
+		printf("문자열을 입력하세요 : ");
+		get(temp);
+		sp[i] = (char *)malloc(sizeof(char) * strlen(temp) + )
+
+	}
 	return 0;
 }
