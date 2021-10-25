2859f1398bd84437911098ac4def2001e7b08f97
brixxt27 <brixxt27@gmail.com>
2021-10-25 19:58:05 +0900

    edit ex16-5

--- hongong/chapter16/ex16-5.c
+++ hongong/chapter16/ex16-5.c
@@ -2,7 +2,48 @@
 #include <string.h>
 #include <stdlib.h>
 
+void	ft_putstr(char **ps);
+void	ft_free(char **ps);
+
 int	main(void)
 {
 	char	temp[80];
-	
+	char	*str[21] = {0};
+	int		i = 0;
+
+	while (i < 20)
+	{
+		printf("문자열을 입력하세요 : ");
+		fgets(temp, 80, stdin);
+		if (strcmp(temp, "end\n") == 0)
+			break;
+		str[i] = (char *)malloc(sizeof(char) * strlen(temp) + 1);
+		strcpy(str[i], temp);
+		i++;
+	}
+	ft_putstr(str);
+	ft_free(str);
+	return 0;
+}
+
+void	ft_putstr(char **ps)
+{
+	int		i = 0;
+
+	while (ps[i] != NULL)
+	{
+		printf("%s", ps[i]);
+		i++;
+	}
+}
+
+void	ft_free(char **ps)
+{
+	int		i = 0;
+
+	while (ps[i] != NULL)
+	{
+		free(ps[i]);
+		i++;
+	}
+}
