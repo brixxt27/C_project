9f7ee4be1cddcdb1ac08a0bed6026f8f9382e16c
brixxt27 <brixxt27@gmail.com>
2021-10-26 16:35:47 +0900

    add ch17

---
+++ hongong/chapter17/ex17-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct profile
+{
+	int		age;
+	double	height;
+};
+
+struct student
+{
+	struct profile	pf;
+	int				id;
+	double			grade;
+};
+
+int	main(void)
+{
+
+	return 0;
+}
