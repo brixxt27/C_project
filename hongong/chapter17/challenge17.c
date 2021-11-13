a3fa42f8b3066d984f6a9bdb0a6aadf000289108
brixxt27 <brixxt27@gmail.com>
2021-11-14 03:01:47 +0900

    add challenge17.c

---
+++ hongong/chapter17/challenge17.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+typedef struct	s_student t_student;
+struct			s_student
+{
+	int		num;
+	char	name[10];
+	int		point[3];
+	int		total;
+	double	avg;
+	char	grade;
+};
+void	input_data(void);
+char	cal_grade(double d);
+void	print_data(t_student *pt);
+
+int	main(void)
+{
+	input_data();
+	return 0;
+}
+
+void	input_data(void)
+{
+	t_student	list[5];
+	int			i;
+
+	i = 0;
+	while (i < 5)
+	{
+		printf("학번 : ");
+		scanf("%d", &list[i].num);
+		printf("이름 : ");
+		scanf("%s", list[i].name);
+		printf("국어, 영어, 수학 점수 : ");
+		scanf("%d", &list[i].point[0]);
+		scanf("%d", &list[i].point[1]);
+		scanf("%d", &list[i].point[2]);
+		list[i].total = list[i].point[0] + list[i].point [1] + list[i].point[2];
+		list[i].avg = list[i].total / 3.0;
+		list[i].grade = cal_grade(list[i].avg);
+		i++;
+	}
+	print_data(list);
+}
+
+void	print_data(t_student *pt)
+{
+	int i;
+
+	i = 0;
+	printf("#정렬 전 데이터...\n");
+	while (i < 5)
+	{
+		printf("%d	", pt[i].num);
+		printf("%s	", pt[i].name);
+		printf("%d	", pt[i].point[0]);
+		printf("%d	", pt[i].point[1]);
+		printf("%d	", pt[i].point[2]);
+		printf("%d	", pt[i].total);
+		printf("%.1lf	", pt[i].avg);
+		printf("%c\n", pt[i].grade);
+		i++;
+	}
+}
+
+char			cal_grade(double d)
+{
+	char c;
+
+	if (d >= 90)
+		c = 'A';
+	else if (d >= 80)
+		c = 'B';
+	else if (d >= 70)
+		c = 'C';
+	else
+		c = 'F';
+	return (c);
+}
