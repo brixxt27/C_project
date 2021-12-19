7b3deaab24ac457822bc5c28effd76c51c06b827
jayoon <jayoon@student.42seoul.kr>
2021-12-19 16:50:16 +0900

    Proceed my project

--- learn_C/ex17.c
+++ learn_C/ex17.c
@@ -26,15 +26,67 @@ struct Connection {
 void	die(const char *message);
 struct	Connection *Database_open(const char *filename, char mode);
 void	Database_load(struct Connection *conn);
+void	Database_write(struct Connection *conn);
+void	Databases_get(struct Connection *conn, int id);
+void	Database_set(struct Connection *conn, int id, const char *name, const char *email);
+void	Address_print(struct Address *addr);
+void	Database_delete(struct Connection *conn, int id);
+void	Database_list(struct Connection *conn);
+void	Database_close(struct Connection *conn);
 
 int	main(int argc, char *argv[])
 {
 	if (argc < 3)
 		die("USAGE: ex17 <dbfile> <action> [action params]");
 
-	char *filename = argv[1];
-	char action = argv[2][0];
-	struct Connection *conn = Database_open(filename, action);
+	char	*filename = argv[1];
+	char	action = argv[2][0];
+	struct	Connection *conn = Database_open(filename, action);
+	int		id = 0;
+
+	if (argc > 3) id = atoi(argv[3]);
+	if (id >= MAX_ROWS) die("There's not that many records.");
+
+	switch (action) {
+		case 'c':
+			Databases_create(conn);
+			Database_write(conn);
+			break;
+
+		case 'g':
+			if (argc != 4)
+				die("Need an id to get");
+
+			Database_get(conn, id);
+			break;
+
+		case 's':
+			if (argc != 6)
+				die("Need an id to get");
+
+			Database_set(conn, id, argv[4], argv[5]);
+			Database_write(conn);
+			break;
+
+		case 'd':
+			of (argc != 4)
+				die("Need id to delete");
+
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+
+		case 'l':
+			Database_list(conn);
+			break;
+
+		default:
+			die("Invalid action: c=create, g=get, s=set, d=del, l=list");
+	}
+
+	Database_close(conn);
+
+	return (0);
 }
 
 void	die(const char *message)
@@ -81,4 +133,84 @@ void	Database_load(struct Connection *conn)
 	int rc = fread(conn -> db, sizeof(struct Database), 1, conn -> file);
 	if (rc != 1)
 		die("Failed to load database.");
+}
+
+void	Database_write(struct Connection *conn)
+{
+	rewind(conn -> file);
+
+	// rc 는 하나의 flag 변수로 fwrite와 fflush 함수의 반환값을 판별하는 변수
+	int rc = fwrite(conn -> db, sizeof(struct Database), 1, conn -> file);
+	if (rc != 1)
+		die("Failed to write database.");
+
+	rc = fflush(conn -> file);
+	if (rc == -1)
+		die("Cannot flush database.");
+}
+
+void	Databases_get(struct Connection *conn, int id)
+{
+	struct Address *addr = &conn -> db -> rows[id];
+
+	if (addr -> set) {
+		Address_print(addr);
+	}
+	else {
+		die("ID is not set");
+	}
+}
+
+void	Database_set(struct Connection *conn, int id, const char *name, const char *email)
+{
+	struct Address *addr = &conn -> db -> rows[id];
+	if (addr -> set)
+		die("Already set, delete it first");
+
+	addr -> set = 1;
+	// 경고: 버그임. "프로그램 깨뜨리기" 섹션을 읽고 고칠 것
+	char *res = strncpy(addr -> name, name, MAX_DATA);
+	//strncpy 버그를 보여줌
+	if (!res)
+		die("Name copy failed");
+
+	res = strncpy(addr -> email, email, MAX_DATA);
+	if (!res)
+		die("Email copy failed");
+}
+
+void	Address_print(struct Address *addr);
+{
+	printf("%d %s %s\n", addr -> id, addr -> name, addr -> email);
+}
+
+void	Database_delete(struct Connection *conn, int id)
+{
+	struct Address addr = {.id = id, .set = 0};
+	conn -> db -> rows[id] = addr;
+}
+
+void	Database_list(struct Connection *conn)
+{
+	int i = 0;
+	struct Database *db = conn -> db;
+
+	for (i = 0; i < MAX_ROWS; i++) {
+		struct Address *cur = &db -> rows[i];
+
+		if (cur -> set) {
+			Address_print(cur);
+		}
+	}
+}
+
+void	Database_close(struct Connection *conn)
+{
+	if (conn) {
+		if (conn -> file)
+			fclose(conn -> file);
+		if (conn -> db)
+			free(conn -> db);
+		free(conn);
+	}
 }
