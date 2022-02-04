bd0ea877a87696f7333f4e615cc5d5ad99228621
brixxt27 <brixxt27@gmail.com>
2022-02-04 22:31:46 +0900

    hanoiTowerMove

--- data_structures/chapter02/HonoiTowerSolu.c
+++ data_structures/chapter02/HonoiTowerSolu.c
@@ -1,8 +1,10 @@
 /*
-	if num == 1 이면 하나만 옮기고 끝
-	n~2 까지 n-1 개 을 by 로 이동 시킨다.
-	1 을 to로 이동시킨다.
-	n~2 까지 n-1 개를 to로 이동시킨다.
+	if num == 1 이면
+		하나만 옮기고 끝
+	else
+		n~2 까지 n-1 개 을 by 로 이동 시킨다.
+		1 을 to로 이동시킨다.
+		n~2 까지 n-1 개를 to로 이동시킨다.
    */
 
 #include <stdio.h>
@@ -11,7 +13,7 @@ void	HanoiTowerMove(int num, char from, char by, char to);
 
 int	main(void)
 {
-	HanoiTowerMove(3, A, B, C);
+	HanoiTowerMove(3, 'A', 'B', 'C');
 	return (0);
 }
 
@@ -23,6 +25,8 @@ void	HanoiTowerMove(int num, char from, char by, char to)
 	}
 	else
 	{
-		printf("원반 %d을 %c 에서 %c 로 옮긴다.\n", num, from, by);
+		HanoiTowerMove(num - 1, from, to, by);
+		printf("원반 %d을 %c 에서 %c 로 옮긴다.\n", num, from, to);
+		HanoiTowerMove(num - 1, by, from, to);
 	}
 }
