7283a81b8a72c75b5270fa596991981a51a1766c
brixxt27 <brixxt27@gmail.com>
2022-02-05 23:58:34 +0900

    Proceed my project

--- baekjoon/weeks_5/1874.c
+++ baekjoon/weeks_5/1874.c
@@ -1,9 +1,165 @@
 /*
- *	몇 개를 다룰 것인지 n 을 입력한다.
- *	원하는 순서의 수열 1이상 n이하의 정수를 순서대로 입력한다.
- *	while 수열의 가장 아래 수 < 오름차순까지 push 한다.
- *		+ 출력
- *	이후 해당 요소를 팝한다.
- *		- 출력
- *
+   	<개요>
+   	이 문제는 총 세 개의 영역이 있다고 생각하면 된다. 오름차순 영역, 스택영역, 수열 영역 이렇게 세 개 이다.
+
+   	<풀이>
+   	몇 개 입력 받을지 n에 입력 받음
+   	for n
+   		수열을 이루는 수 차례대로 입력. 이를 순서대로 동적할당한 arr[i] 에 넣음
+   	i = 0; // 배열 arr 의 인덱스
+   	while n != i
+   		if 스택에 아무 것도 없거나 || stack_top < arr[i]
+   			n부터 arr[i]까지 push
+		else  stack_top == arr[i];
+			pop;
+			i++;
+		else (stack_top > arr[i])
+			NO 출력
+			break;
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE	1
+#define FALSE	0
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node * next;
+} Node;
+
+typedef struct _listStack
+{
+	Node * head;
+} ListStack;
+
+
+typedef ListStack Stack;
+
+void StackInit(Stack * pstack);
+int SIsEmpty(Stack * pstack);
+
+void SPush(Stack * pstack, Data data);
+Data SPop(Stack * pstack);
+Data SPeek(Stack * pstack);
+
+int	main(void)
+{
+	int		n;
+	int		num = 1;
+	int		*pa;
+	char	*op;
+	int		i, j, k = 0;
+	int		flag = 0;
+	Stack	stack;
+
+	scanf("%d", &n);
+	pa = (int *)malloc(n * sizeof(int));
+	op = (char *)malloc((2 * n + 1) *sizeof(char));
+	if (!pa || !op)
+		return (-1);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &pa[i]);
+	}
+	i = 0;
+	while (n != i)
+	{
+		if (SIsEmpty(&stack) || stack.head->data < pa[i])
+		{
+			for (; num <= pa[i]; num++)
+			{
+				SPush(&stack, num);
+				op[k] = '+';
+				k++;
+			}
+		}
+		else if (stack.head->data == pa[i])
+		{
+			SPop(&stack);
+			op[k] = '-';
+			k++;
+			i++;
+		}
+		else
+		{
+			printf("NO\n");
+			flag = 1;
+			break ;
+		}
+	}
+	if (flag == 0)
+	{
+		op[k] = '\0';
+		k = 0;
+		while (op[k])
+		{
+			printf("%c\n", op[k]);
+			k++;
+		}
+	}
+	while(!SIsEmpty(&stack))
+		SPop(&stack);
+	free(pa);
+	free(op);
+	return (0);
+}
+
+/********************/
+/*		stack		*/
+/********************/
+void StackInit(Stack * pstack)
+{
+	pstack->head = NULL;
+}
+
+int SIsEmpty(Stack * pstack)
+{
+	if(pstack->head == NULL)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack * pstack, Data data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+
+	newNode->data = data;
+	newNode->next = pstack->head;
+
+	pstack->head = newNode;
+}
+
+Data SPop(Stack * pstack)
+{
+	Data rdata;
+	Node * rnode;
+
+	if(SIsEmpty(pstack)) {
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	rdata = pstack->head->data;
+	rnode = pstack->head;
+
+	pstack->head = pstack->head->next;
+	free(rnode);
+
+	return rdata;
+}
+
+Data SPeek(Stack * pstack)
+{
+	if(SIsEmpty(pstack)) {
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	return pstack->head->data;
+}
